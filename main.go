c0e8dccd0957b3de8c54b636328a2f35668df350
wanglr0705 <168050404+wanglr0705@users.noreply.github.com>
2024-06-25 16:44:33 +0800

    主函数

--- main.go
+++ main.go
@@ -2,28 +2,36 @@ package main
 
 import (
 	"context"
+	"go_xorm_mysql_redis/config"
+	local_caching "go_xorm_mysql_redis/local-caching"
 	"go_xorm_mysql_redis/mysql"
 	"go_xorm_mysql_redis/redis"
 	"go_xorm_mysql_redis/router"
+	"go_xorm_mysql_redis/utils"
 )
 
 func main() {
-	ctx := context.Background()
-	newRouter := router.NewRouter(ctx, mysql.Engine, redis.RDB)
-	newRouter.RouterRegistration()
-}
+	cfg := config.Config{}
+	// 从YAML文件中获取配置信息
+	utils.GetYaml(&cfg)
 
-// 初始化
-func init() {
 	//连接mysql
-	err := mysql.Mysql()
+	err := mysql.Mysql(&cfg)
 	if err != nil {
 		panic(err)
 	}
 
 	//连接redis
-	err = redis.Redis()
+	err = redis.Redis(&cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	//创建本地缓存
+	local_caching.LocalCaching()
+
+	ctx := context.Background()
+	//注册路由
+	newRouter := router.NewRouter(ctx, &cfg, mysql.Engine, redis.RDB, local_caching.Cache)
+	newRouter.RouterRegistration()
 }
